2d69b0ec398942665c5479690d2b3e6960958aae
松島悠人 <>
2021-02-23 12:23:19 +0900

    edit file

--- ChallengeTask10/ViewController.swift
+++ ChallengeTask10/ViewController.swift
@@ -13,13 +13,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     var selectedIndex: IndexPath!
 
-    let prefectures: [String] = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県","島根県","岡山県", "広島県","山口県","徳島県","香川県","愛媛県","高知県", "福岡県","佐賀県", "長崎県", "熊本県", "大分県", "宮崎県","鹿児島県", "沖縄県"]
+    private let prefectures: [String] = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県","島根県","岡山県", "広島県","山口県","徳島県","香川県","愛媛県","高知県", "福岡県","佐賀県", "長崎県", "熊本県", "大分県", "宮崎県","鹿児島県", "沖縄県"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-
         let nib = UINib(nibName: "prefectureTableViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "Cell") 
     }
@@ -29,22 +28,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! prefectureTableViewCell
         cell.label1.text = prefectures[selectedIndex.row]
         cell.label2.text =  "\(selectedIndex.row)番目の都道府県です"
-
         if selectedIndex.row % 3 == 0 {
-            cell.backgroundColor = UIColor.red
-
+            cell.backgroundColor = #colorLiteral(red: 1, green: 0.8206560612, blue: 0.8154960871, alpha: 1)
         } else if selectedIndex.row % 3 == 1 {
-            cell.backgroundColor = UIColor.blue
-
+            cell.backgroundColor = #colorLiteral(red: 0.8721580505, green: 0.967730701, blue: 0.6013033986, alpha: 1)
         }  else if selectedIndex.row % 3 == 2 {
-            cell.backgroundColor = UIColor.yellow
+            cell.backgroundColor = #colorLiteral(red: 0.7769456506, green: 0.8170322776, blue: 0.9654322267, alpha: 1)
         }
-
         return cell
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         prefectures.count
     }
-    }
+}
 
--- ChallengeTask10/prefectureTableViewCell.swift
+++ ChallengeTask10/prefectureTableViewCell.swift
@@ -11,15 +11,8 @@ class prefectureTableViewCell: UITableViewCell {
 
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var label2: UILabel!
-    override func awakeFromNib() {
-        super.awakeFromNib()
-
-    }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-    
 }
